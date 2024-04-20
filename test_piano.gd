eb93c9c2543b907bedc311d72745965679626139
pillowtrucker <githubino@janitor.chat>
2024-04-20 16:51:40 +0200

    add one compact piano (already rigged), one grand piano with removable keys + external rigged keys for animation

---
+++ test_piano.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var ok = get_node("Sketchfab_Scene");
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- tooting.gd
+++ tooting.gd
@@ -12,13 +12,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	elapsed += delta;
-	var i = int(elapsed);
-	#var rng = RandomNumberGenerator.new();
-	#var i = rng.randi_range(1,3);
-	if i % 2 == 0:
-		super.play_note(Toot.ShittyNote.E);
-	elif i % 3 == 0:
-		super.play_note(Toot.ShittyNote.G);
-	else:
-		super.play_note(Toot.ShittyNote.C)
+	pass
+#	elapsed += delta;
+#	var i = int(elapsed);
+#	var since_last_sec = int(elapsed * 1000) % 1000;
+#	if (0 <= since_last_sec) and (since_last_sec <= 16):
+#		if i % 2 == 0:	
+#			super.play_note(Toot.ShittyNote.E);
+#		elif i % 3 == 0:
+#			super.play_note(Toot.ShittyNote.G);
+#		else:
+#			super.play_note(Toot.ShittyNote.C)
+#	else:
+#		super.depress_note(Toot.C);
+#		super.depress_note(Toot.G);
+#		super.depress_note(Toot.E);
